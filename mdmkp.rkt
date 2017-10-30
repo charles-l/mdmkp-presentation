148b06c9ab99deddf56e4a9c40b58d8e35023fb6
ncharlie <charles.saternos@gmail.com>
2017-10-30 11:46:19 -0400

    finalized (for now)

--- mdmkp.rkt
+++ mdmkp.rkt
@@ -56,26 +56,12 @@
        (item "Hybrid"))
 
 (slide #:title "JAYA"
-       (item "JAYA (R. venkata Rao 2016")
+       (item "JAYA (R. venkata Rao 2016)")
        (item "Parameter-less")
        (item "Population based")
        (item "Single phased")
        (scale ($$ "R1, R2 \\in \\{0, 1\\}") 3/4)
        (scale ($$ "X_{jki} = X_{jki} + R1_{jki} [X_{jBESTi} - X_{jki}] - R2_{jki}[X_{jWORSTi} - X_{jki}]") 3/4))
-
-(slide #:title "Repair"
-       (t "After each iteration of JAYA repair to improve feasibility")
-       (scale ($$ "obj = 10 x_1 + 20 x_2 + 30 x_3 + 40 x_4 + 50 x_5 + 60 x_6 + 70 x_7 + 80 x_8 ") 4/5)
-       (scale ($$ " 5 x_1 +  20 x_2 +  25 x_3 +  35 x_4 + 40 x_5 + 45 x_6 + 55 x_7 +  60 x_8 \\leq 150") 4/5)
-       (scale ($$ "90 x_1 + 120 x_2 +  70 x_3 + 110 x_4 + 90 x_5 + 65 x_6 + 80 x_7 + 150 x_8 \\leq 300") 4/5)
-       (scale ($$ " 5 x_1 +  20 x_2 + 100 x_3 +  35 x_4 + 60 x_5 + 45 x_6 + 50 x_7 +  60 x_8 \\geq 80") 4/5)
-       (scale ($$ "90 x_1 +  60 x_2 +  70 x_3 + 110 x_4 + 90 x_5 + 45 x_6 + 20 x_7 +  10 x_8 \\geq 200") 4/5)
-       'next
-       (scale (tt "(0 1 0 0 1 0 0 1) violation = 100") 4/5)
-       'next
-       (scale (tt "(0 1 0 0 1 0 0 0) violation = 50") 4/5)
-       'next
-       (scale (tt "(0 1 1 0 1 0 0 0) feasible obj = 100") 4/5))
 
 (slide #:title "Approach"
        (item "Initial population generation")
@@ -101,6 +87,29 @@
        (tt "(0 1 0 1 0)")
        (tt "(1 0 0 0 1)"))
 
+(slide #:title "Repair"
+       (t "After each iteration of JAYA repair to improve feasibility")
+       (scale ($$ "obj = 10 x_1 + 20 x_2 + 30 x_3 + 40 x_4 + 50 x_5 + 60 x_6 + 70 x_7 + 80 x_8 ") 4/5)
+       (scale ($$ " 5 x_1 +  20 x_2 +  25 x_3 +  35 x_4 + 40 x_5 + 45 x_6 + 55 x_7 +  60 x_8 \\leq 150") 4/5)
+       (scale ($$ "90 x_1 + 120 x_2 +  70 x_3 + 110 x_4 + 90 x_5 + 65 x_6 + 80 x_7 + 150 x_8 \\leq 300") 4/5)
+       (scale ($$ " 5 x_1 +  20 x_2 + 100 x_3 +  35 x_4 + 60 x_5 + 45 x_6 + 50 x_7 +  60 x_8 \\geq 80") 4/5)
+       (scale ($$ "90 x_1 +  60 x_2 +  70 x_3 + 110 x_4 + 90 x_5 + 45 x_6 + 20 x_7 +  10 x_8 \\geq 200") 4/5)
+       'next
+       (scale (tt "(0 1 0 0 1 0 0 1) violation = 100") 4/5)
+       'next
+       (scale (tt "(0 1 0 0 1 0 0 0) violation = 50") 4/5)
+       'next
+       (scale (tt "(0 1 1 0 1 0 0 0) feasible obj = 100") 4/5))
+
+(slide #:title "Results"
+       (item "JAYA"
+             (item "Average objective 76351")
+             (item "737/810 feasible"))
+       (item "JAYA with repair"
+             (item "Average objective 83683")
+             (item "800/810 feasible")))
+
 (slide #:title "Conclusions"
        (item "MDMKP problems can be addressed with metaheuristics")
-       (item "JAYA is a simple, parameterless, and efficient solution"))
+       (item "JAYA is a simple, parameterless, and efficient solution")
+       (item "Results are massively improved with repair"))
